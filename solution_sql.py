1195e290ae8eea22bd41bf564020e17b44346dbb
theashishmalik <imashishmalik.45@gmail.com>
2019-06-30 12:31:25 +0530

    defined authors function

--- solution_sql.py
+++ solution_sql.py
@@ -2,6 +2,40 @@
 
 import psycopg2
 
+
+def popularArticles(query)
+    article_query='''select title, rslt.count from (
+        select path, count(path) from (
+            select * from log where status = '200 OK' and path != '/'
+            ) as tbl
+            group by path order by count(path) desc limit 3
+        ) as rslt
+        join articles on rslt.path like '%' || slug || '%' order by
+        rslt.count desc;'''
+    query.execute(article_query)
+    for (name,count) in query.fetchall():
+        print("{} - {} views ".format(name,count))
+    print("-" * 80)
+
+ def popularAuthors(query)
+    authors_query='''select name, sum(rslt_two.count) as num
+        from (select author, rslt.count from (
+        select path, count(path) from (
+            select * from log where status = '200 OK' and path != '/'
+            ) as tab
+            group by path
+        ) as rslt
+        join articles on rslt.path like '%' || slug || '%') as rslt_two
+        join authors on rslt_two.author = authors.id
+        group by name order by num desc;'''
+    query.execute(authors_query)
+    for (name, count) in query.fetchall():
+        print("    {} - {} views".format(name, count))
+    print("-" * 80)
+
+
+
+
 if __name__=="__main__"
     try:
         db=psycopg2.connect(database="news")
