4c98070d5f84fe8648b106a2fe85cb7bd5632c57
theashishmalik <imashishmalik.45@gmail.com>
2019-06-30 11:40:22 +0530

    updated try block

--- solution_sql.py
+++ solution_sql.py
@@ -1,8 +1,15 @@
-#!/usr/bin/env 
+#!/usr/bin/env python3
 
 import psycopg2
 
 if __name__=="__main__"
     try:
         db=psycopg2.connect(database="news")
-        
+        query=db.cursor()
+        print("The list of most famous articles are as follows : \n")
+        popularArticles(query)
+        print("The most Popular Authors are: \n")
+        popularAuthors(query)
+        print("Days in which more than 1% of requests lead to errors: \n")
+        errorDays(query)
+        db.close()
